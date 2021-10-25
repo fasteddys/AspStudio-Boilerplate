ec262584af294c11d426250d20599ba0bed5f872
Vincent Lauro <vinniehat@gmail.com>
2021-10-25 09:19:27 -0400

    Initial

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AspStudio_Boilerplate.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspStudio_Boilerplate.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)  
+        {  
+        }  
+    }
+}
