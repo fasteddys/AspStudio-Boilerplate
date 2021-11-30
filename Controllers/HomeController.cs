a95fce27687d67c363b6eac41efbc804d795899b
Vincent Lauro <vinniehat@gmail.com>
2021-11-30 09:15:32 -0500

    Added FullName to the header

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace AspStudio_Boilerplate.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
