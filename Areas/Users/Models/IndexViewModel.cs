7545e752517d8984be2bf03054b7073646772321
Vincent Lauro <vinniehat@gmail.com>
2021-12-02 09:33:55 -0500

    Users Area is now working sorta

---
+++ Areas/Users/Models/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using AspStudio_Boilerplate.Models;
+
+namespace AspStudio_Boilerplate.Areas.Users.Models;
+
+public class IndexViewModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string CreatedAt { get; set; }
+}
