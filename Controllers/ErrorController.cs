51e082f96ca152a59e7645ec0e7dd1638ff87450
Vincent Lauro <vinniehat@gmail.com>
2021-12-01 21:53:26 -0500

    Added 404 ErrorPage Redirection

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AspStudio_Boilerplate.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorController : Controller
+    {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult ErrorPage()
+        {
+            return View();
+        }
+    }
+}
